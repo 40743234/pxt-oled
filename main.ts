4a773ad57a4c5485687116370f62194b90785e0f
40743234 <53332980+40743234@users.noreply.github.com>
2019-11-11 19:06:52 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -83,8 +83,7 @@ namespace lumexoled {
        var array0 = src.split(',').map(parseInt);
         var src1 = "1,3,5,4,3,5,6";
        var array1 = src1.split(',').map(parseInt);
-        var src2 = "1,2,5,4,3,0,9,9";
-       var array2 = src2.split(',').map(parseInt);
+       var array2 = ['10','1','100'].map(function(x) { return parseInt(x, 10); });
         switch(del) { 
          case mycharacter.type1: return array0;
          case mycharacter.type2: return array1;
