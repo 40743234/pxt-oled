74c31c940a93bb4c4b74423bc4ac6584029b4f55
40743234 <53332980+40743234@users.noreply.github.com>
2021-09-25 14:37:43 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -153,6 +153,13 @@ namespace lumexoled {
         serial.readUntil("E")
         basic.pause(10)
     }
+    
+    //% blockId="OLED_putNumber" block="OLED put number: %myNumber|size: %mySize|on line: %line|column: %column|display %showState"
+    //% weight=84 blockGap=0 blockExternalInputs=true line.min=0 line.max=7 column.min=0 column.max=20
+    export function putNumber(myNumber: number, mySize: fontSize, line: number, column: number, showState: showNow): void {
+        putString(myNumber.toString(), mySize, line, column, showState)
+    }
+    
     export function putString(myStr: string, mySize: fontSize, line: number, column: number, showState: showNow): void {
         if (myStr.length > 0) {
             //sending = true
